13908e0a93ed24c215bb877a1aea624c7934cf0c
jl118 <jen.liebelt@gmail.com>
2021-12-14 17:35:52 -0800

    added foreign and primary keys

--- db/schema.sql
+++ db/schema.sql
@@ -5,15 +5,17 @@ create database cms_db;
 use cms_db;
 
 create table department (
-    id int not null auto_increment,
+    id int not null auto_increment primary key,
     name varchar(30) not null
 );
 
 create table roles (
-    id int not null,
+    id int not null primary key,
     title varchar(30) not null,
     salary decimal not null,
     dept_id int,
+    foreign key (dept_id)
+    references department (id)
     on delete set null
 );
 
@@ -23,5 +25,9 @@ create table employee (
     last_name varchar(30) not null,
     role_id int,
     manager_id int,
+    foreign key (role_id)
+    references roles (id)
+    foreign key (manager_id)
+    references employee (id)
     on delete set null
 )
