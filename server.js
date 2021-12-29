727283a60bb878636d90b7eab089216c50304205
jl118 <jen.liebelt@gmail.com>
2021-12-29 18:42:29 -0500

    worked on server.js and file structure

--- server.js
+++ server.js
@@ -2,3 +2,23 @@
 const inquirer = require("inquirer");
 const cTable = require("console.table");
 
+const db = require("./config/connect");
+
+
+// TODO: view all departments function
+
+// TODO: view all roles function
+
+// TODO: view all employees function
+
+// TODO: add department function
+
+// TODO: add role function
+
+// TODO: add employee function
+
+// TODO: update employee function
+
+// TODO: add inquirer prompts to perform different actions
+
+// TODO: initialize the application
