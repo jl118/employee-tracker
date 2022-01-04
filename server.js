5a3fd4651ad2b5ada4fc8fcba2fde4d0688cdbf8
jl118 <jen.liebelt@gmail.com>
2022-01-04 15:13:37 -0500

    worked on server.js

--- server.js
+++ server.js
@@ -7,32 +7,53 @@ const db = require("./config/connect");
 
 // TODO: view all departments function
 function viewAllDepts() {
-
+    db.query('select * from departments', function (err, results) {
+        console.table(results);
+    });
 };
 
 // TODO: view all roles function
 function viewAllRoles() {
-
+    db.query('select roles.id, roles.title, roles.salary, departments.name FROM roles JOIN departments ON departments.id = roles.dept_id;', function (err, results) {
+        console.table(results);
+    });
 };
 
 // TODO: view all employees function
 function viewAllEmps() {
-
+    db.query('select e.id, concat(e.first_name, " ", e.last_name) as "Name", roles.title, departments.name, roles.salary, concat(m.first_name, " ", m.last_name) as "Manager Name" from employees e left join employees m on e.manager_id = m.id join roles on e.roles_id = roles.id join departments on departments.id = roles.dept_id order by e.id asc;', function (err, results) {
+        console.table(results);
+    });
 };
 
 // TODO: add department function
 function addDepartment() {
-
+    inquirer
+        .prompt(
+            [{
+                
+            }]
+        )
 };
 
 // TODO: add role function
 function addRole() {
-
+    inquirer
+        .prompt(
+            [{
+                
+            }]
+        )
 };
 
 // TODO: add employee function
 function addEmployee() {
-
+    inquirer
+        .prompt(
+            [{
+                
+            }]
+        )
 };
 
 // TODO: update employee function
@@ -79,6 +100,6 @@ function init() {
                 case "Exit": process.exit(1);
             }
         })
-}
+};
 
 // TODO: initialize the application
